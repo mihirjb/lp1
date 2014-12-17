0cea09c7a6ffbd94de8934367e4ac8793b1fd119
Mihir Buch <mihirjb@gmail.com>
2014-12-17 11:34:04 +0800

    added some sort of security

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,2 +1,7 @@
 class Signup < ActiveRecord::Base
+  
+   validates_presence_of :email
+   validates_uniqueness_of :email, case_sensitive: false
+   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+   validates_presence_of :fullname  
 end
